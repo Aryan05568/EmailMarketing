c4b8024201233f1bde9abbd4222290d65c04e1ac
Sameer Srivastava <anilanita07@gmail.com>
2025-06-27 00:09:20 +0530

    env changed

--- Client/src/utility/config.js
+++ Client/src/utility/config.js
@@ -1,2 +1,2 @@
-// export const BASEURL = "https://emailmarketing-3qin.onrender.com"
-export const BASEURL = "http://ec2-3-110-219-99.ap-south-1.compute.amazonaws.com:5000"
+export const BASEURL = "https://emailmarketing-3qin.onrender.com"
+// export const BASEURL = "http://ec2-3-110-219-99.ap-south-1.compute.amazonaws.com:5000"
